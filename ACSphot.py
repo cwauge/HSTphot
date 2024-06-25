34860e6e2830bba924d31519cc415bc4854f7d5e
Connor Auge <connor.auge@gmail.com>
2024-06-25 11:49:09 -1000

    New class to perform photometry on ACS data

--- ACSphot.py
+++ ACSphot.py
@@ -1,2 +1,15 @@
 import numpy as np
 from scipy.stats import std
+from astropy.io import fits
+from astropy.wcs import WCS
+
+
+
+
+
+class ACS_data():
+
+    def __init__(self,data_array):
+        self.data = data_array
+
+
